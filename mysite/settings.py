59144ccac94caf967db380cd8cc53ed83c58fc6f
Gwonchanwoo <ghee1321@naver.com>
2026-05-18 17:00:39 +0900

    chore: 비밀번호 등 민감 정보 .env 파일로 분리 및 환경설정 보안 강화

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,16 +11,19 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv(os.path.join(BASE_DIR, '.env'))
+
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-w3w_i*)ltam%b_@-+_&xxbpu!)_#=kxffgf&(4$pucc_9xi83p'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -81,7 +84,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'capstone_db',       # 방금 성공적으로 만들어진 DB 이름
         'USER': 'root',              # MySQL 접속 아이디 (보통 root)
-        'PASSWORD': 'zxc053077', # ⬅️ 꼭 본인 비밀번호로 바꿔주세요!
+        'PASSWORD': os.environ.get('DB_PASSWORD'), # ⬅️ 꼭 본인 비밀번호로 바꿔주세요!
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
